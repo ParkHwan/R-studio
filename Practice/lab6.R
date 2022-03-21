f97410641837a29c65dc5b213b28ae8974aa4dc2
power080900 <power080900@gmail.com>
2022-03-21 14:41:57 +0900

    0321

--- Practice/lab6.R
+++ Practice/lab6.R
@@ -1,12 +1,10 @@
-rm(list = ls())
-
 #문제1
 exam1 <- function(){
   x<-LETTERS[]
   y<-letters[]
-  paste(x,y,sep="")
+  return(paste(x,y,sep=""))
 }
-print(exam1())
+exam1()
 
 #문제2
 exam2 <- function(x){
@@ -14,67 +12,63 @@ exam2 <- function(x){
   for(i in 1:x){
     y <- y+i
   }
-  cat("함수 호출 결과 :",y)
+  return(y)
 }
-exam2(100)
+cat("함수호출 결과 :",exam2(100))
 
 #문제3
 exam3 <- function(x,y){
-  x <- x
-  y <- y
   if (x > y){
     a <- x-y
-    return(a)
   } else if (x < y) {
     a <- y-x
-    return(a)
   } else {
     a <- 0
-    return(a)
   }
-  print("함수호출 결과 :",a)
+  return(a)
 }
-exam3(30,10)
+print(paste("함수 호출 결과 :",exam3(30,10)))
 
 #문제4
-exam4 <- function(x,y,z) {
-  x <- x
-  y <- y
-  z <- z
-  if (y == "+"){
-    result <- x+z
-  } else if (y == "-"){
-    result <- x-z
-  } else if (y == "*"){
-    result <- x*z
-  } else if (y == "%/%") {
-    if (x == 0) {
+exam4 <- function(num1,oper,num2) {
+  if (oper == "+"){
+    result <- num1+num2
+  } else if (oper == "-"){
+    result <- num1-num2
+  } else if (oper == "*"){
+    result <- num1*num2
+  } else if (oper == "%/%") {
+    if (num1 == 0) {
       result <- "오류1"
-    } else if (z==0) {
+    } else if (num2==0) {
       result <- "오류2"
     } else {
-      result <- x %/% z
+      result <- num1 %/% num2
     }
-  } else if (y == "%%") {
-    if (x == 0) {
+  } else if (oper == "%%") {
+    if (num1 == 0) {
       result <- "오류1"
-    } else if (z==0) {
+    } else if (num2==0) {
       result <- "오류2"
     } else {
-      result <- x %% z
+      result <- num1 %% num2
     }
+  } else {
+    result <- "규격의 연산자만 전달하세요"
   }
+  return(result)
 }
 print(exam4(3,"+",1))
 print(exam4(3,"-",1))
 print(exam4(3,"*",1))
 print(exam4(3,"%/%",1))
 print(exam4(3,"%%",1))
+print(exam4(3,"_",1))
+print(exam4(0,"%%",1))
+print(exam4(1,"%%",0))
 
 #문제5
 exam5 <- function(renum,basic="#") {
-  renum <- renum
-  basic <- basic
   if (renum > 0 ){
     for (i in 1:renum) {
       cat(basic)
