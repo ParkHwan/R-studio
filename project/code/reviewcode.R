b0b8286867ebeb316de2e8b2e08bf814a7b28d0c
power080900 <power080900@gmail.com>
2022-04-06 16:35:11 +0900

    0406

--- project/code/reviewcode.R
+++ project/code/reviewcode.R
@@ -1,3 +1,4 @@
+library(RSelenium)
 remDr <- remoteDriver(remoteServerAddr = "localhost" , 
                       port = 4445,
                       browserName = "chrome")
@@ -5,9 +6,12 @@ loginPage <- "https://www.jobplanet.co.kr/users/sign_in?_nav=gb"
 url <- "https://www.jobplanet.co.kr/companies?sort_by=review_avg_cache&city_id=1"
 remDr$open()
 remDr$navigate(loginPage)
+
 ID <-remDr$findElement(using="css selector", "#user_email")
+ID$clickElement()
 
 PW <- remDr$findElement(using="css selector", "#user_password")
+PW$clickElement()
 
 loginbtn <- remDr$findElement(using='css selector', "section.section_email.er_r > fieldset > button")
 loginbtn$clickElement()
