804823336c5c0d9d98f44157679ac186215d453f
power080900 <power080900@gmail.com>
2022-04-10 13:40:57 +0900

    0410

--- project/code/preprocessingcode.R
+++ project/code/preprocessingcode.R
@@ -17,5 +17,5 @@ joindata2 <- full_join(jobPlanetData,saraminData,by='기업명')
 View(joindata1)
 View(joindata2)
 
-write.csv(joindata2,"project/output/jobdata1.csv")
+write.csv(joindata1,"project/output/jobdata1.csv")
 write.csv(joindata2,"project/output/jobdata2.csv")
