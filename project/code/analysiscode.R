1b3a5c7ad6efaffb2856af84287c79da06cdf016
power080900 <power080900@gmail.com>
2022-04-10 13:42:22 +0900

    0410

--- project/code/analysiscode.R
+++ project/code/analysiscode.R
@@ -2,6 +2,14 @@ library(dplyr)
 
 jobData1 <- read.csv("project/output/jobdata1.csv")
 jobData2 <- read.csv("project/output/jobdata2.csv")
+jobData3 <- read.csv("project/output/jobdata3.csv")
 
 jobData1
 jobData2
+jobData3
+
+filter <- jobData3 %>% 
+  filter(as.numeric(jobData3$평점) >= 3.0)
+
+hist(jobData3$평점)
+hist(filter$평점)
--- project/code/preprocessingcode.R
+++ project/code/preprocessingcode.R
@@ -13,9 +13,11 @@ View(saraminData)
 
 joindata1 <- inner_join(jobPlanetData,saraminData,by='기업명')
 joindata2 <- full_join(jobPlanetData,saraminData,by='기업명')
+joindata3 <- left_join(jobPlanetData,saraminData,by='기업명')
 
 View(joindata1)
 View(joindata2)
 
 write.csv(joindata1,"project/output/jobdata1.csv")
 write.csv(joindata2,"project/output/jobdata2.csv")
+write.csv(joindata3,"project/output/jobdata3.csv")
